2bb112e8b96397c856089d30c6e27e79fc096426
Mrudul-Bhatt <mrudul1203bhatt@gmail.com>
2020-06-12 11:05:51 +0530

    search added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState, useRef } from 'react';
 import { Link, useHistory, NavLink } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import { useDispatch, useSelector } from 'react-redux';
 import * as actions from '../store/actions/user';
+import SearchIcon from '@material-ui/icons/Search';
+import M from 'materialize-css';
+
 const Navbar = () => {
+	const searchModal = useRef(null);
+	const [search, setSearch] = useState('');
+	const [users, setUsers] = useState([]);
 	const dispatch = useDispatch();
 	//const [SUA, setSUA] = useState(null);
 	const logout = () => dispatch(actions.logout());
@@ -19,8 +25,77 @@ const Navbar = () => {
 	// }, [navChange]);
 	const history = useHistory();
 
+	useEffect(() => {
+		M.Modal.init(searchModal.current);
+	}, []);
+
+	const searchUsers = (query) => {
+		setSearch(query);
+		fetch('/search', {
+			method: 'post',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: 'Bearer ' + localStorage.getItem('jwt'),
+			},
+			body: JSON.stringify({
+				query,
+			}),
+		})
+			.then((res) => res.json())
+			.then((value) => {
+				console.log(value);
+				setUsers(value.result);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
 	return (
 		<nav>
+			<div
+				id='modal1'
+				className='modal'
+				ref={searchModal}
+				style={{ color: 'black' }}
+			>
+				<div className='modal-content'>
+					<input
+						type='text'
+						placeholder='Search'
+						value={search}
+						onChange={(e) => searchUsers(e.target.value)}
+					/>
+					<ul className='collection'>
+						{users
+							? users.map((item) => {
+									return (
+										<Link
+											key={item._id}
+											to={
+												item._id !== user._id ? '/user/' + item._id : '/profile'
+											}
+											onClick={() => {
+												M.Modal.getInstance(searchModal.current).close();
+												setSearch('');
+											}}
+										>
+											<li className='collection-item'>{item.email}</li>
+										</Link>
+									);
+							  })
+							: null}
+					</ul>
+				</div>
+				<div className='modal-footer'>
+					<button
+						onClick={() => setSearch('')}
+						className='modal-close waves-effect waves-green btn-flat'
+					>
+						Done
+					</button>
+				</div>
+			</div>
 			<div
 				className='nav-wrapper white'
 				style={{
@@ -38,6 +113,15 @@ const Navbar = () => {
 					<ul id='nav-mobile' className='right '>
 						{user ? (
 							<div>
+								<li>
+									<i
+										className='large material-icons modal-trigger'
+										data-target='modal1'
+										style={{ color: 'blue', fontSize: '30px' }}
+									>
+										search
+									</i>
+								</li>
 								<li>
 									<Link to='/home'>Home</Link>
 								</li>
---
+++ src/components/Screens/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+	return (
+		<div>
+			<h1>Landing Page Of Instagram</h1>
+		</div>
+	);
+};
+
+export default LandingPage;
